fbad428a52476566801ed8b861539df2a49176e3
Shaunticlair <shaunticlair@gmail.com>
2025-03-31 12:20:05 -0700

    Update to present

--- MITgcm_Processing/open_files.py
+++ MITgcm_Processing/open_files.py
@@ -7,7 +7,7 @@ workingdir = '/nobackup/sruiz5/MITGCMTEST/ECCOV4/release4/run'
 os.chdir(workingdir)
 
 # Define iteration number(s) to read
-iter_number = 00001  # Example iteration number
+iter_number = 9  # Example iteration number
 
 ### State variables
 
@@ -19,11 +19,11 @@ zos_data = mds.rdmds('Eta', itrs=iter_number)  # Sea surface height
 
 ### Boundary conditions
 
-bottom_pressure = mds.rdmds('diags/OBP_mon_mean/OBP_mon_mean') # Ocean bottom pressure
-tau_x = mds.rdmds('diags/EXFtaux_mon_mean/EXFtaux_mon_mean') # x-axis wind stress
-tau_y = mds.rdmds('diags/EXFtauy_mon_mean/EXFtauy_mon_mean') # y-axis wind stress
-heat_flux = mds.rdmds('diags/oceQnet_mon_mean/oceQnet_mon_mean') #Diabatic heat flux
-freshwater_flux = mds.rdmds('diags/oceFWflx_mon_mean/oceFWflx_mon_mean') #Surface mass flux
+bottom_pressure = mds.rdmds('diags/OBP_mon_mean/OBP_mon_mean', itrs=iter_number) # Ocean bottom pressure
+tau_x = mds.rdmds('diags/EXFtaux_mon_mean/EXFtaux_mon_mean', itrs=iter_number) # x-axis wind stress
+tau_y = mds.rdmds('diags/EXFtauy_mon_mean/EXFtauy_mon_mean', itrs=iter_number) # y-axis wind stress
+heat_flux = mds.rdmds('diags/oceQnet_mon_mean/oceQnet_mon_mean', itrs=iter_number) #Diabatic heat flux
+freshwater_flux = mds.rdmds('diags/oceFWflx_mon_mean/oceFWflx_mon_mean', itrs=iter_number) #Surface mass flux
 
 # If you need to read multiple iterations:
 # iter_numbers = [10000, 20000, 30000]
