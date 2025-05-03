dde803332c66856d466ff1bae2d0fe2104feccf2
Shaunticlair <swr@mit.edu>
2025-05-02 21:23:15 -0400

    Flipped the spatial slice as a test

--- Samudra_Testing/time_lag.py
+++ Samudra_Testing/time_lag.py
@@ -359,8 +359,8 @@ if __name__ == "__main__":
     # Example usage
     print('None')
     results = main( 
-        reference_point=(90, 180),
-        spatial_slice=(slice(90-1,90+1+1), slice(180-1,180+1+1)),#(slice(50, 130), slice(110, 250)),
+        reference_point=(180,90),
+        spatial_slice=(slice(180-1,180+1+1), slice(90-1,90+1+1)),#(slice(50, 130), slice(110, 250)),
         time_window=72*2,  # Number of time steps for correlation
         max_lag=72,      # Maximum lag in time steps
         lag_step=6,      # Step size for lags
